955b4b184dc3a1e0abe8ca17c786e72281fef423
NatalyaPs <n0905@yandex.ru>
2023-07-15 13:54:05 +0300

    add hw 1

---
+++ hw_1/index.js
@@ -0,0 +1,12 @@
+let degreesCelsius;
+let degreesFahrenheit;
+
+degreesCelsius = 20;
+degreesFahrenheit = (9 / 5) * degreesCelsius + 32;
+
+console.log(`${degreesCelsius} degrees Celsius = ${degreesFahrenheit} degrees Fahrenheit`);
+
+
+let name = 'Nata';
+let admin = name;
+console.log(admin);
---
+++ sem_1/index.js
@@ -0,0 +1,57 @@
+console.log("привет, 'студент'")
+console.log('привет из index.js')
+
+// task 2
+const num = 123;
+console.log(num);
+
+const num1 = 123 + 10;
+console.log(num1);
+
+let a;
+a = 5;
+a = 7;
+console.log(a);
+
+let b = 1 + 2 + 3;
+b = 10;
+console.log(b);
+
+let [cc, dd] = [1, 2];
+console.log(cc, dd);      // 1 2
+
+// shift + alt + a - многострочный комментарий
+
+const c = 10;
+const d = 2;
+
+const sum = c + d;
+const sub = c - d;
+const mult = c * d;
+const div = c / d;
+
+const res = sum + sub + mult + div;
+console.log('результат суммы всех переменных равен: ' + res);
+console.log(`Сумма = ${sum}
+Разность = ${sub}
+Произведение = ${mult}
+деление = ${div}`);
+
+
+// задание 5
+const v = 1.5;
+const m = 0.75;
+const n = v + m;
+console.log(n);
+
+const f = 5;
+console.log(f);
+
+const name = 'Nika';
+console.log(name);
+
+const j = 'Java';
+const s = 'Script';
+console.log(j + s);
+console.log(j + ' ' + s);
+console.log(`${j} ${s}`);
